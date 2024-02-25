53acc17470c504c46c1c90968b2d7c22450df076
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 19:05:53 +0500

    Add files via upload

---
+++ q29.ts
@@ -0,0 +1,25 @@
+//question no 29
+/*Favorite Fruit: Make a array of your favorite fruits, and then write a series
+of independent if statements that check for certain fruits in your array. */
+
+let favorite_fruits=["apple","mango","grapes"];
+
+for(let i=0; i<5; i++){
+    if (i==1){
+
+
+    console.log("you really like apple!");
+    }
+    if (i==1){
+        console.log("you really like mango!");
+    }
+    if(i==2){
+        console.log("you really like grapes!");
+    }
+    if(i==3){
+        console.log("no fruit in array");
+    }
+    if(i==4){
+        console.log("no fruit in array");
+    }
+}
