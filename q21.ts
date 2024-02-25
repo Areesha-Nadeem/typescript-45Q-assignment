2e8f39fa7a95eb5881780d2de711eec67c8fb894
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 17:38:36 +0500

    Add files via upload

---
+++ q21.ts
@@ -0,0 +1,15 @@
+//question n 21
+/*They think of something you could store in a TypeScript Object. 
+Write a program that creates Objects containing these items.*/
+
+let person= {
+    person_name: "sidra",
+    age: 19,
+    gender_female: "yes",
+    postal_code: 3452,
+    city:"Lahore"
+
+}
+console.log(person);
+
+
