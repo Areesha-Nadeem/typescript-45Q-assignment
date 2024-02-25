d3ae05c14c8baec42321ad2ca0978761227545ea
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 12:31:57 +0500

    Add files via upload

---
+++ q9.ts
@@ -0,0 +1,8 @@
+//question no 9
+/*Favorite Number: Store your favorite number in a variable. Then,
+using that variable, create a message that reveals your favorite number.
+Print that message.*/
+
+let favrt_num=9;
+let message1=`"my favourite number is ${favrt_num}"`;
+console.log(`${message1} `);
