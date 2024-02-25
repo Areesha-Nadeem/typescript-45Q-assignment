4e2a9e060e3c34bd6081b356bac42e2f3a2863ba
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 17:33:05 +0500

    Add files via upload

---
+++ q12.ts
@@ -0,0 +1,9 @@
+/*question no 12
+Greetings: Start with the array you used in Exercise 11, but instead of just
+printing each person’s name, print a message to them. The text of each message 
+should be the same, but each message should be personalized with the 
+person’s name.
+*/
+let namearray=["laiba", "Esha"," Hina"];
+for(let i=0;i<namearray.length;i++)
+console.log(`Hello!`, namearray[i] , `How are you?\n`);
