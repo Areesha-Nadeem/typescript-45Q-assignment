f99eabf12b5312aa38ec8b222043aba9c56e90f0
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 19:08:59 +0500

    Add files via upload

---
+++ q39.ts
@@ -0,0 +1,13 @@
+//question no 39
+/*City Names: Write a function called city_country() that takes in the
+ name of a city and its country........ */
+
+ function city_country(city:string,country:string): any{
+
+       console.log(city,country);
+       return;
+
+ }
+ city_country("'Lahore,'","'Pakistan'")
+ city_country("'Paris',","'France'")
+ city_country("'New York',","'USA'")
