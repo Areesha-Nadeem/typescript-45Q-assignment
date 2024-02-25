0945092320545620fe0b18e2e8ebd0e5354f9543
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 12:30:19 +0500

    Add files via upload

---
+++ q4.ts
@@ -0,0 +1,6 @@
+//Question no 04
+/*Famous Quote: Find a quote from a famous person you admire. Print
+the quote and the name of its author.Your output should look something 
+like the following, including the quotation marks: */
+
+console.log(`Albert Einstein once said,"you never fail until you stop trying."`);
