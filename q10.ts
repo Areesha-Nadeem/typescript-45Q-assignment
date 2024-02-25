1823e1d27ec858cabfa1902c330f8295aa5066b2
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 12:32:23 +0500

    Add files via upload

---
+++ q10.ts
@@ -0,0 +1,20 @@
+//question no 10
+/*Adding Comments: Choose two of the programs you’ve written, and add at
+least one comment to each. If you don’t have anything specific to write
+because your programs are too simple at this point,just add your name 
+and the current date at the top of each program file. Then write one 
+sentence describing what the program does.*/
+
+//subtract two numbers
+let n3=10;
+let n2=2;
+let rslt= n2-n3;
+console.log(rslt);
+/*
+Name: areesha Nadeem
+Date: jan 1,2023
+divide two numbers*/
+let a=64;
+let b=8;
+let res= a/b;
+console.log(res);
