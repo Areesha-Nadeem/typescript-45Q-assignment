cc9a857729128b9e975443d85100c02dff90e0c8
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 19:03:49 +0500

    Add files via upload

---
+++ q24.ts
@@ -0,0 +1,43 @@
+//questin no 24
+/*More Conditional Tests: You don’t have to limit the number of tests you 
+create to 10. If you want to try more comparisons, write more tests. */
+
+let naam1:string="daanya" 
+let nam2:string="zoye";
+console.log("is nam1==zoye ? | predict false");
+console.log(naam1=='zoye');
+
+console.log("is nam2=zoye ? | predict false");
+console.log(nam2=='zoye');
+
+
+
+let rr=30;
+console.log("is rr>=25 ? | predict true");
+console.log(rr==30);
+
+console.log("is rr<=25s ? | predict false");
+console.log(rr==10);
+
+console.log('is nam1=daanya && nam2=zoye ? | predict true');
+console.log(naam1=='daanya' && nam2=='zoye');
+
+
+console.log("\n chcek the if statement");
+if(naam1=='daanya' || nam2=='ali'){
+console.log(false);}
+
+
+//test wheather is not a array
+console.log('\n the items in array');
+let aa1:string[]=['a','b','c'];
+console.log(aa1.includes('a'));
+
+
+
+
+//Test whether an item is not  a array
+console.log("\n check is array r not");
+let nam4=[]? 'true':'false';
+console.log(nam4=='ali');
+
