41cd84c0de41a66c59d296d42cd5a5beb9caf645
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 12:31:21 +0500

    Add files via upload

---
+++ q7.ts
@@ -0,0 +1,29 @@
+// question no 7
+/*Number Eight: Write addition, subtraction, multiplication, and division
+operations that each result in the number 8. Be sure to enclose
+your operations in print  statements to see the results.*/
+// let num1=5;
+// let num2=3;
+// let result= num1+num2;
+// console.log(result);
+
+function sum(num1:number,num2:number){
+    return num1+num2;
+}
+console.log(5+3);
+
+//for subtraction
+let num=10;
+let num3=2;
+let result1= num-num3;
+console.log(result1);
+//for multiplication
+let num4=2;
+let num5=4;
+let result2= num4*num5;
+console.log(result2);
+//for division
+let num1=64;
+let num2=8;
+let result= num1/num2;
+console.log(result);
