b9e4146b6702a0c303a414dada69d62ea8f9767b
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 17:37:09 +0500

    Add files via upload

---
+++ q19.ts
@@ -0,0 +1,13 @@
+//question no 19
+/*Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+print a message indicating the number of people you are inviting to dinner. */
+
+
+let dinnerguest=[ "LAIBA", "ESHA", "HINA", "KINZA","IMRA"];
+for(var i=0;i<dinnerguest.length;i++){
+    i=i;
+
+}
+console.log("the number of guest that invite for dinner =",i);
+
+
