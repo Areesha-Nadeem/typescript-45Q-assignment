649e99eb24d57d2fc8ba8555af94bf9f6dce97d0
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 17:37:36 +0500

    Add files via upload

---
+++ q20.ts
@@ -0,0 +1,9 @@
+//question no 20
+/*Think of something you could store in a array. For example, you could make 
+a list of mountains, rivers, countries, cities, languages, or anything
+else you’d like.Write a program that creates a list containing these items. */
+
+let arr=["Turkey", "UK", "Korea", "Paris"];
+console.log(`list of countries is:
+ {Germany,Canada,${arr}}`);
+
