37fff5fd6416957aae542c47c65f9cf74bfdec63
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 12:30:38 +0500

    Add files via upload

---
+++ q5.ts
@@ -0,0 +1,9 @@
+//Question no 5
+/*Famous Quote 2: Repeat Exercise 4, but this time store the famous 
+person’s name in a variable called famous_person. Then compose your message 
+and store it in a new variable called message. Print your message.
+*/
+
+let famous_person= "Albert Einstein";
+let message= "you never fail until you stop trying.";
+console.log(`${famous_person}once said,"${message}"`);
