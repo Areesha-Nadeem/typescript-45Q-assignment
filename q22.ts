9ea8c1bb5d987988d9a95640d6e5733512da28b1
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 17:39:04 +0500

    Add files via upload

---
+++ q22.ts
@@ -0,0 +1,10 @@
+//question no 22
+/*Intentional Error: If you haven’t received an array index error in one of your
+programs yet, try to make one happen. Change an index in one of your programs
+to produce an index error. Make sure you correct the error before closing the program.
+*/
+
+let inarray=["choco","jam","jug","juice"]
+console.log("index error: ", inarray[4]);
+/*in this array you have 4 items in list but you acces 5th one item so  
+the index error is occurs*/
