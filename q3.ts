5cc99fe56f3ce2775b0c38ccb42319cb3fffdb0b
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 12:29:58 +0500

    Add files via upload

---
+++ q3.ts
@@ -0,0 +1,13 @@
+//Question no 3
+/* Name Cases: Store a person’s name in a variable, and then
+ print that person’s name in lowercase, uppercase, and titlecase.
+*/
+
+
+
+let n ="ArEeSha";
+let upper_case= n.toUpperCase();
+let lower_case= n.toLowerCase();
+console.log (`name = ${n}`);
+console.log (`In upper case= ${upper_case}`);
+console.log(`In lower case= ${lower_case}`);
