ab159e42c6fc4ea0a68bd83131f705efdb575af7
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 19:04:57 +0500

    Add files via upload

---
+++ q27.ts
@@ -0,0 +1,49 @@
+//questin no 27
+/*Alien Colors #3: Turn your if-else chain from Exercise 5-4 into
+ an if-else chain. */
+
+let alien_color3="red";
+
+ /*If the alien is green, print a message that the player earned 5 points.*/
+if(alien_color3=="green"){
+    console.log("the player earn 5 points");
+}
+// If the alien is yellow, print a message that the player earned 10 points.
+if(alien_color3=="yellow"){
+    console.log("the player earn 10 points");
+}
+
+// If the alien is red, print a message that the player earned 15 points.
+if(alien_color3=="red"){
+    console.log("the player earn 15 points");
+}
+
+
+
+let alien_color4="green";
+if(alien_color4=="green"){
+    console.log("the player earn 5 points");
+}
+
+if(alien_color4=="yellow"){
+    console.log("the player earn 10 points");
+}
+
+if(alien_color4=="red"){
+    console.log("the player earn 15 points");
+}
+
+
+
+let alien_color5="yellow";
+if(alien_color5=="green"){
+    console.log("the player earn 5 points");
+}
+
+if(alien_color5=="yellow"){
+    console.log("the player earn 10 points");
+}
+
+if(alien_color5=="red"){
+    console.log("the player earn 15 points");
+}
