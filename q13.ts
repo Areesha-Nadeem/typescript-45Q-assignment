1e9e8659a04aef2cd616158951c0cc39eaa51aba
Areesha_Nadeem <151401063+Areesha-Nadeem@users.noreply.github.com>
2024-02-25 17:34:18 +0500

    Add files via upload

---
+++ q13.ts
@@ -0,0 +1,23 @@
+/*question no 13
+Your Own Array: Think of your favorite mode of transportation, such as a 
+motorcycle or a car, and make a list that stores several examples. Use your
+list to print a series of statements about these items, such as
+“I would like to own a Honda motorcycle.”
+*/
+let favrtarray=["heavybike","OD","hyundai"];
+for (let j=0; j<favrtarray.length; j++){
+    console.log (`i would like to own a`,favrtarray[j]);
+
+}
+
+// let transportmode=["car", "motorcycle"];
+// for(let i=0; i<transportmode.length; i++){
+// let type="bike";
+// if (type=transportmode[1]){
+// console.log(`my favrt type is :${type}`);
+// }
+// else if(type !=transportmode[1]){
+// console.log(`my favrt type is :`,type);
+// }
+
+// }
---
+++ q14.ts
@@ -0,0 +1,12 @@
+//question no 14
+/*Guest List: If you could invite anyone, living or deceased, to dinner, 
+who would you invite? Make a list that includes at least three people you’d
+like to invite to dinner.Then use your list to print a message to each person,
+inviting them to dinner.*/
+
+let guest=["laiba","esha","hina"];
+console.log (`Guest name: ${guest}\n`);
+for(let i=0; i<guest.length; i++){
+    console.log (`Hey! whatsup?`, guest[i], `I would be delighted if you join me for a lovely dinner `);
+}
+
